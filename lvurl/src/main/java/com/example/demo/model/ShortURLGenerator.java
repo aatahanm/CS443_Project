ef70c1852325d866ffc0a0880a83d041c9416c10
dogacar <43743196+dogacar@users.noreply.github.com>
2020-04-23 01:26:49 +0300

    Add files via upload

--- lvurl/src/main/java/com/example/demo/model/ShortURLGenerator.java
+++ lvurl/src/main/java/com/example/demo/model/ShortURLGenerator.java
@@ -3,6 +3,14 @@ package com.example.demo.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.IOException;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 @Document
 public class ShortURLGenerator {
 
@@ -52,9 +60,40 @@ public class ShortURLGenerator {
 		return number;
 	}
 	
-	private String generator(int number)
+	private static String generator(int number)
 	{
-		return Integer.toString(number);
+		// Map to store 62 possible characters
+		char map[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+
+		StringBuffer shorturl = new StringBuffer();
+
+		while (number > 0) {
+			shorturl.append(map[number % 62]);
+			number = number / 62;
+		}
+		String input = shorturl.reverse().toString();
+		try {
+			// Static getInstance method is called with hashing MD5
+			MessageDigest md = MessageDigest.getInstance("MD5");
+
+			// digest() method is called to calculate message digest
+			//  of an input digest() return array of byte
+			byte[] messageDigest = md.digest(input.getBytes());
+
+			// Convert byte array into signum representation
+			BigInteger no = new BigInteger(1, messageDigest);
+
+			// Convert message digest into hex value
+			String hashtext = no.toString(16);
+			while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+			return hashtext;
+		}
+		// For specifying wrong message digest algorithms
+		catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	// add Duration time ability
 	
@@ -71,4 +110,9 @@ public class ShortURLGenerator {
 		return userName;
 	}
 
+	public static void main (String[] args){
+		int n = 12345;
+		String shorturl = generator(n);
+		System.out.println("Generated short url in MD5: " + shorturl);
+	}
 }
