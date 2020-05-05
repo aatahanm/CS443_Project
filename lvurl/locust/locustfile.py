d4257abbffa69803c22fd3991987cfcfab5e6129
yusufsamsum <yusuf.samsum@ug.bilkent.edu.tr>
2020-05-05 23:04:33 +0300

    locust testing

--- lvurl/locust/locustfile.py
+++ lvurl/locust/locustfile.py
@@ -5,13 +5,17 @@ body = {
   "name": "string"
 }
 
-headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0MTJiYSIsImV4cCI6MTU4ODcyNzk3MiwiaWF0IjoxNTg4NjkxOTcyfQ.tajNRqODf_6TK_nW3Ai-gBqMYJxPMve8dLDDnBel2Ig'}
 
 def get(l):
-    l.client.get("/getAll?userName=yusuf")
+    l.client.get("/ahmet")
+
+def login(l):
+	l.client.post("/auth/Authenticate", {"username" : "test12ba" ,
+											"password" : "test12ba"})
 
 def save(l):
-    l.client.post("/create?userName=ali&longURL=ali.com&number=5", data=json.dumps(body), headers=headers)
+    l.client.post("/create/URL?userName=test12ba&longURL=www.facebook.com", headers=headers)
 
 class UserBehavior(TaskSet):
     tasks = {get: 100, save: 1}
